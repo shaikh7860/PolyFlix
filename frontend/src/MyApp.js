2f49fc9f8493a86e20ccea3332fb2ffc4e686ae5
blyon03 <benbenlyon@gmail.com>
2022-10-28 15:53:18 -0700

    search bar implemented

--- frontend/src/MyApp.js
+++ frontend/src/MyApp.js
@@ -8,7 +8,7 @@ import Profile from "./Pages/Profile";
 import Movie from "./Pages/Movie";
 import ErrorPage from "./Pages/ErrorPage";
 import SearchResult from './Pages/SearchResult';
-
+import SearchBar from './SearchBar';
 
 
 
@@ -16,6 +16,8 @@ import SearchResult from './Pages/SearchResult';
 function MyApp() {
   const [characters, setCharacters] = useState([]);
   const [movies, setmovies] = useState([]);
+  let [searchResults, setResults] = useState([...movies]);
+  const [searchInput, setSearchInput] = useState('');
   const navigate = useNavigate()
   
   async function fetchAll(){
@@ -80,14 +82,36 @@ function removeOneCharacter (index) {
       navigate('/users-table')
    });
 }
+
+function searchForMovies(movie) {
+   setSearchInput(movie);
+   fetchSome(movie).then(result => {
+     if (result)
+       setResults(result);
+   });
+   navigate('/searchResult');  
+   return;
+ }
+
+ async function fetchSome(name){
+    try {
+        const response = await axios.get('http://localhost:5001/?name=' + name);
+        return response.data;
+    }
+    catch (error){
+        console.log(error);
+        return false;
+    }
+ }
+
  return (
   <div className="container">
    <nav>
       <ul>
          <li><Link to='/profile'>Go to Profile</Link></li>
          <li><Link to='/'>Go Home</Link></li>
-         <li><Link to='/searchResult'>Search</Link></li>
       </ul>
+      <SearchBar handleSubmit={searchForMovies}/>
       </nav>
     {/* <Table characterData={characters} removeCharacter={removeOneCharacter} />
     <Form handleSubmit = {updateList} /> */}
@@ -97,7 +121,12 @@ function removeOneCharacter (index) {
          <Route path="/" element={<Home movieData={movies} characterData={characters} removeCharacter={removeOneCharacter} handleSubmit = {updateList} />} />
          <Route path="/profile" element={<Profile />} />
          <Route path="/movie/:movieName" element={<Movie />} />
-         <Route path="/searchResult" element={<SearchResult />} />
+         <Route path="/searchResult" element={
+         <SearchResult movieData={searchResults} 
+         characterData={characters} 
+         removeCharacter={removeOneCharacter}
+         movieName={searchInput}/>
+         } />
          <Route path="*" element={<ErrorPage />}/>
       </Routes>
   
--- frontend/src/Pages/SearchResult.js
+++ frontend/src/Pages/SearchResult.js
@@ -1,7 +1,18 @@
-import React from "react";
 
-function SearchResult() {
-  return <div>THIS IS THE SearchResult PAGE</div>;
+import React from "react";
+import Table from '../Table.js'
+
+function SearchResult(props) {
+  if (props.movieData.length > 0){
+
+    return <div>Search results for: {props.movieName}
+
+    <Table movieData = {props.movieData} characterData={props.characterData} removeCharacter={props.removeCharacter} />
+    </div>;
+  }else{
+    return <div>No results found for: {props.movieName}</div>
+  }
+
 }
 
 export default SearchResult;
