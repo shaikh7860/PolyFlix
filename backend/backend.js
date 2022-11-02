72f83a3f4ef3f58f0f0a08420242bae1c2afae5d
blyon03 <benbenlyon@gmail.com>
2022-11-02 15:13:08 -0700

    Updated changes

--- backend/backend.js
+++ backend/backend.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 5001;
-const cors = require('cors');
+const cors = require("cors");
 const crypto = require("crypto");
 const axios = require("axios");
-require('dotenv').config();
+require("dotenv").config();
 
-const userServices = require('./models/user-services');
+const userServices = require("./models/user-services");
 
 app.use(cors());
 app.use(express.json());
@@ -15,64 +15,70 @@ app.use(express.json());
 //     res.send('Hello World!');
 // });
 
-app.get('/', async (req, res) => {
-    try{
-        const name = req.query['name'];
-        if (name){
-            const result = await axios.request('https://api.themoviedb.org/3/search/movie/?api_key=' + process.env.API_KEY + '&query=' + name)
-            res.send(result.data.results);
-        }else{
-            const result = await axios.request('https://api.themoviedb.org/3/movie/popular?api_key=' + process.env.API_KEY + '&language=en-US&page=1')
-            res.send(result.data.results);
-        }
-    } catch(error){
-            console.log(error);
-            res.status(500).send('An error ocurred in the server.');
+app.get("/", async (req, res) => {
+  try {
+    const name = req.query["name"];
+    if (name) {
+      const result = await axios.request(
+        "https://api.themoviedb.org/3/search/movie/?api_key=" +
+          process.env.API_KEY +
+          "&query=" +
+          name
+      );
+      res.send(result.data.results);
+    } else {
+      const result = await axios.request(
+        "https://api.themoviedb.org/3/movie/popular?api_key=" +
+          process.env.API_KEY +
+          "&language=en-US&page=1"
+      );
+      res.send(result.data.results);
     }
-});
-app.get('/users', async (req, res) => {
-    const name = req.query['name'];
-    const job = req.query['job'];
-    try {
-        const result = await userServices.getUsers(name, job);
-        res.send({users_list: result});         
-    } catch (error) {
-        console.log(error);
-        res.status(500).send('An error ocurred in the server.');
-    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("An error ocurred in the server.");
+  }
 });
 
-app.get('/users/:id', async (req, res) => {
-    const id = req.params['id'];
-    const result = await userServices.findUserById(id);
-    if (result === undefined || result === null)
-        res.status(404).send('Resource not found.');
-    else {
-        res.send({users_list: result});
-    }
+app.get("/users", async (req, res) => {
+  const name = req.query["name"];
+  const job = req.query["job"];
+  try {
+    const result = await userServices.getUsers(name, job);
+    res.send({ users_list: result });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("An error ocurred in the server.");
+  }
 });
 
-app.post('/users', async (req, res) => {
-    const user = req.body;
-    const savedUser = await userServices.addUser(user);
-    if (savedUser)
-        res.status(201).send(savedUser);
-    else
-        res.status(500).end();
+app.get("/users/:id", async (req, res) => {
+  const id = req.params["id"];
+  const result = await userServices.findUserById(id);
+  if (result === undefined || result === null)
+    res.status(404).send("Resource not found.");
+  else {
+    res.send({ users_list: result });
+  }
 });
 
-app.delete('/users/:id', async (req, res) => {
-    const id = req.params['id'];
-    const result = await userServices.removeUserById(id);
-    if (result == -1){
-       res.status(404).send('Resource not found.');
-    }
-    else{
-       res.status(204).end();
-    }
- 
- });
+app.post("/users", async (req, res) => {
+  const user = req.body;
+  const savedUser = await userServices.addUser(user);
+  if (savedUser) res.status(201).send(savedUser);
+  else res.status(500).end();
+});
+
+app.delete("/users/:id", async (req, res) => {
+  const id = req.params["id"];
+  const result = await userServices.removeUserById(id);
+  if (result == -1) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.status(204).end();
+  }
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});      
+  console.log(`Example app listening at http://localhost:${port}`);
+});
--- frontend/src/MyApp.js
+++ frontend/src/MyApp.js
@@ -1,140 +1,132 @@
 // import Table from './Table';
 // import Form from './Form';
-import axios from 'axios';
-import React, {useState, useEffect} from 'react';
-import {Routes, Route, Link, useNavigate } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 import Home from "./Pages/Home";
 import Profile from "./Pages/Profile";
 import Movie from "./Pages/Movie";
 import ErrorPage from "./Pages/ErrorPage";
-import SearchResult from './Pages/SearchResult';
-import SearchBar from './SearchBar';
-
-
-
+import SearchResult from "./Pages/SearchResult";
+import SearchBar from "./SearchBar";
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
   const [movies, setmovies] = useState([]);
   let [searchResults, setResults] = useState([...movies]);
-  const [searchInput, setSearchInput] = useState('');
-  const navigate = useNavigate()
-  
-  async function fetchAll(){
+  const [searchInput, setSearchInput] = useState("");
+  const navigate = useNavigate();
+
+  async function fetchAll() {
     try {
-       const response = await axios.get('http://localhost:5001/');
-       return response.data;     
-    }
-    catch (error){
-       //We're not handling errors. Just logging into the console.
-       console.log(error); 
-       return false;         
-    }
- }
-
- useEffect(() => {
-   fetchAll().then( result => {
-     if (result)
-         setmovies(result);
-   });
- }, [] );
-
-
-
-  async function makePostCall(person){
-    try {
-       const response = await axios.post('http://localhost:5001/users', person);
-       return response;
-    }
-    catch (error) {
-       console.log(error);
-       return false;
-    }
- }
- 
-function removeOneCharacter (index) {
-
-   makeDeleteCall(characters[index]['_id']).then( result => {
-      if (result && result.status === 204){
-            const updated = characters.filter((character, i) => {
-            return i !== index
-         });
-         setCharacters(updated);      
-         }
-      });
-    
- }
- async function makeDeleteCall(id){
-   try {
-      const response = await axios.delete(`http://localhost:5001/users/${id}`);
-      return response;
-   }
-   catch (error) {
+      const response = await axios.get("http://localhost:5001/");
+      return response.data;
+    } catch (error) {
+      //We're not handling errors. Just logging into the console.
       console.log(error);
       return false;
-   }
-}
- function updateList(person) { 
-   makePostCall(person).then( result => {
-   if (result && result.status === 201)
-      setCharacters([...characters, result.data] );
-      navigate('/users-table')
-   });
-}
+    }
+  }
 
-function searchForMovies(movie) {
-   setSearchInput(movie);
-   fetchSome(movie).then(result => {
-     if (result)
-       setResults(result);
-   });
-   navigate('/searchResult');  
-   return;
- }
+  useEffect(() => {
+    fetchAll().then((result) => {
+      if (result) setmovies(result);
+    });
+  }, []);
 
- async function fetchSome(name){
+  async function makePostCall(person) {
     try {
-        const response = await axios.get('http://localhost:5001/?name=' + name);
-        return response.data;
+      const response = await axios.post("http://localhost:5001/users", person);
+      return response;
+    } catch (error) {
+      console.log(error);
+      return false;
     }
-    catch (error){
-        console.log(error);
-        return false;
-    }
- }
+  }
 
- return (
-  <div className="container">
-   <nav>
-      <ul>
-         <li><Link to='/profile'>Go to Profile</Link></li>
-         <li><Link to='/'>Go Home</Link></li>
-      </ul>
-      <SearchBar handleSubmit={searchForMovies}/>
+  function removeOneCharacter(index) {
+    makeDeleteCall(characters[index]["_id"]).then((result) => {
+      if (result && result.status === 204) {
+        const updated = characters.filter((character, i) => {
+          return i !== index;
+        });
+        setCharacters(updated);
+      }
+    });
+  }
+  async function makeDeleteCall(id) {
+    try {
+      const response = await axios.delete(`http://localhost:5001/users/${id}`);
+      return response;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+  function updateList(person) {
+    makePostCall(person).then((result) => {
+      if (result && result.status === 201)
+        setCharacters([...characters, result.data]);
+      navigate("/users-table");
+    });
+  }
+
+  function searchForMovies(movie) {
+    setSearchInput(movie);
+    fetchSome(movie).then((result) => {
+      if (result) setResults(result);
+    });
+    navigate("/searchResult");
+    return;
+  }
+
+  async function fetchSome(name) {
+    try {
+      const response = await axios.get("http://localhost:5001/?name=" + name);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  return (
+    <div className="container">
+      <nav>
+        <ul>
+          <li>
+            <Link to="/profile">Go to Profile</Link>
+          </li>
+          <li>
+            <Link to="/">Go Home</Link>
+          </li>
+        </ul>
+        <SearchBar handleSubmit={searchForMovies} />
       </nav>
-    {/* <Table characterData={characters} removeCharacter={removeOneCharacter} />
+      {/* <Table characterData={characters} removeCharacter={removeOneCharacter} />
     <Form handleSubmit = {updateList} /> */}
-   
 
       <Routes>
-         <Route path="/" element={<Home movieData={movies} />} />
-         <Route path="/profile" element={<Profile />} />
-         <Route path="/movie/:movieName" element={<Movie />} />
-         <Route path="/searchResult" element={
-         <SearchResult movieData={searchResults} 
-         characterData={characters} 
-         removeCharacter={removeOneCharacter}
-         movieName={searchInput}/>
-         } />
-         <Route path="*" element={<ErrorPage />}/>
+        <Route path="/" element={<Home movieData={movies} />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/movie/:movieName" element={<Movie />} />
+        <Route
+          path="/searchResult"
+          element={
+            <SearchResult
+              movieData={searchResults}
+              characterData={characters}
+              removeCharacter={removeOneCharacter}
+              movieName={searchInput}
+            />
+          }
+        />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
-  
-   </div>
-
-
-);
+    </div>
+  );
 }
 
 export default MyApp;
