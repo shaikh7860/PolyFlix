611431291b49fcba246144d035d98ecc5d2c99b8
Ali <ali.shaikh@hotmail.com>
2022-11-18 22:32:09 -0800

    deployed frontend app making calls to the deployed backend URL

--- backend/backend.js
+++ backend/backend.js
@@ -11,9 +11,9 @@ const userServices = require("./models/user-services");
 app.use(cors());
 app.use(express.json());
 
-// app.get('/', (req, res) => {
-//     res.send('Hello World!');
-// });
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 
 app.get("/movies/popular", async (req, res) => {
   try {
--- frontend/src/MyApp.js
+++ frontend/src/MyApp.js
@@ -37,7 +37,7 @@ function MyApp() {
 
   async function fetchPopular() {
     try {
-      const response = await axios.get(baseUrl + "movies/popular"); // https://polyflix.azurewebsites.net/movies/popular http://localhost:5001/movies/popular
+      const response = await axios.get(baseUrl + "movies/popular"); // http://localhost:5001/movies/popular
       return response.data;
     } catch (error) {
       //We're not handling errors. Just logging into the console.
@@ -47,7 +47,7 @@ function MyApp() {
   }
   async function fetchTop() {
     try {
-      const response = await axios.get("http://localhost:5001/movies/top"); // https://polyflix.azurewebsites.net/movies/top  http://localhost:5001/movies/top
+      const response = await axios.get(baseUrl + "movies/top"); // http://localhost:5001/movies/top
       return response.data;
     } catch (error) {
       //We're not handling errors. Just logging into the console.
@@ -57,7 +57,7 @@ function MyApp() {
   }
   async function fetchUpcoming() {
     try {
-      const response = await axios.get("http://localhost:5001/movies/upcoming"); // https://polyflix.azurewebsites.net/movies/upcoming  http://localhost:5001/movies/upcoming
+      const response = await axios.get(baseUrl + "movies/upcoming"); // http://localhost:5001/movies/upcoming
       return response.data;
     } catch (error) {
       //We're not handling errors. Just logging into the console.
@@ -100,7 +100,7 @@ function MyApp() {
   }
   async function makeDeleteCall(id) {
     try {
-      const response = await axios.delete(`http://localhost:5001/users/${id}`); // https://polyflix.azurewebsites.net/users/${id} http://localhost:5001/users/${id}
+      const response = await axios.delete(baseUrl + `users/${id}`); // http://localhost:5001/users/${id}
       return response;
     } catch (error) {
       console.log(error);
@@ -127,7 +127,7 @@ function MyApp() {
   async function fetchSome(name) {
     try {
       const response = await axios.get(
-        "http://localhost:5001/search?name=" + name // https://polyflix.azurewebsites.net/search?name=  http://localhost:5001/search?name=
+        baseUrl + "search?name=" + name // http://localhost:5001/search?name=
       );
       return response.data;
     } catch (error) {
@@ -139,7 +139,7 @@ function MyApp() {
   async function tryLogIn(token) {
     try {
       const response = await axios.get(
-        "http://localhost:5001/users?username=" + // https://polyflix.azurewebsites.net/search?name=  http://localhost:5001/users?username=
+        baseUrl + "users?username=" + // http://localhost:5001/users?username=
           token["username"] +
           "&password=" +
           token["password"]
@@ -158,7 +158,7 @@ function MyApp() {
 
   async function makeAccount(token) {
     try {
-      const response = await axios.post("http://localhost:5001/user", token);
+      const response = await axios.post(baseUrl + "user", token);
       updateToken(response.data);
     } catch (error) {
       console.log(error);
@@ -168,16 +168,16 @@ function MyApp() {
   }
 
   async function addToFavorites(movie) {
-    const response = await axios.put("http://localhost:5001/user/" + cookies.id, movie);
+    const response = await axios.put(baseUrl + "user/" + cookies.id, movie);
     if (response){
-      const userRes = await axios.get("http://localhost:5001/users/" + cookies.id);
+      const userRes = await axios.get(baseUrl + "users/" + cookies.id);
       updateToken(userRes.data.users_list, false);
       return userRes;
     }
   }
 
   async function getFavMovies(id){
-    const userRes = await axios.get("http://localhost:5001/users/" + id);
+    const userRes = await axios.get(baseUrl + "users/" + id);
     if (userRes){
       return userRes.data.users_list["favmovies"];
     }else{
