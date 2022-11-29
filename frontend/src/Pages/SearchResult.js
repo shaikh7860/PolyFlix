cf42db54bb6d5449163bdb51d83aedb7d19ef1ed
Ali <ali.shaikh@hotmail.com>
2022-11-28 17:01:32 -0800

    invalid search and no search page updated

--- frontend/src/Pages/SearchResult.js
+++ frontend/src/Pages/SearchResult.js
@@ -6,6 +6,9 @@ import { useCookies } from "react-cookie";
 
 function SearchResult(props) {
   const navigate = useNavigate();
+  const navigateHome = () => {
+    navigate("/home");
+  };
   const [cookies, setCookie] = useCookies("password");
   if (!cookies.password) {
     navigate("/");
@@ -31,28 +34,38 @@ function SearchResult(props) {
   } else {
     if (props.movieData.length > 0) {
       return (
-        <div class="container-fluid bg_image">
-          <div>
-            <nav>
-              <NavBar handleSubmit={props.handleSubmit}></NavBar>
-            </nav>
-            Search results for: {props.movieName}
-            <Table
-              movieData={props.movieData}
-              characterData={props.characterData}
-            />
-          </div>
-          <div class="invalid-url-text">
-            No results found for: {props.movieName}
-          </div>
-          <div class="empty-search-text2">
-            <a href="https://www.google.com/">Return to the home page!</a>
-            {/* <input
-            name="action"
-            type="submit"
-            value="Home"
-            onclick= "GoToHomePage"
-          /> */}
+        <div>
+          <nav>
+            <NavBar handleSubmit={props.handleSubmit}></NavBar>
+          </nav>
+          Search results for: {props.movieName}
+          <Table
+            movieData={props.movieData}
+            characterData={props.characterData}
+          />
+        </div>
+      );
+    } else {
+      return (
+        // <div>
+        //   <div class="invalid-url-text">
+        //     No results found for: {props.movieName}
+        //   </div>
+        //   <div class="empty-search-text2">
+        //     <a href="https://www.google.com/">Return to the home page!</a>
+        //   </div>
+        // </div>
+        <div class="container-fluid bg_image2">
+          <nav>
+            <NavBar handleSubmit={props.handleSubmit}></NavBar>
+          </nav>
+          <div class="invalid-url-page">
+            <div class="invalid-url-text">
+              No results found for: {props.movieName}
+            </div>
+            <div class="empty-search-text2">
+              <button onClick={navigateHome}>Return to the home page</button>
+            </div>
           </div>
         </div>
       );
